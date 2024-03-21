4962762893303661834a52bb7eebc40da363b08d
Exequiel65 <marcos97britos@gmail.com>
2024-03-21 12:12:35 -0300

    fix ImageMessage and LocationMessage

--- WhatsAppBussinesApi.Dotnet/Structure/Text/ImageMessage.cs
+++ WhatsAppBussinesApi.Dotnet/Structure/Text/ImageMessage.cs
@@ -1,29 +1,70 @@
-﻿namespace WhatsAppBussinesApi.Dotnet.Structure.Text
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WhatsAppBussinesApi.Dotnet.Structure.Text
 {
     public class ImageMessage : BaseMessage
     {
-        public TypeMessage Type { get; set; } = TypeMessage.image;
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TypeMessage type { get; set; } = TypeMessage.image;
 
         public BaseImage image { get; set; }
+
+        public ImageMessage()
+        {
+
+        }
+        public ImageMessage(string to, Uri link)
+        {
+            this.image = new ImageComponent(link.ToString());
+            this.to = to;
+        }
+        public ImageMessage(string to, Uri link, string providerName)
+        {
+            this.to = to;
+            this.image = new ImageComponentWithProvider(link.ToString(), providerName);
+        }
+
+        public ImageMessage(string to, ImageComponent component)
+        {
+            this.to = to;
+            this.image = component;
+        }
+
+        public ImageMessage(string to, string id)
+        {
+            this.to = to;
+                this.image = new ImageComponentWithId(id);
+        }
     }
 
 
     public class BaseImage { }
-    public class ImageComponent : BaseImage
+    public class ImageComponentWithProvider : BaseImage
     {
         public string link { get; set; }
         public BaseProvider provider { get; set; }
-        public ImageComponent() { }
-        public ImageComponent(string link, string providerName)
+        public ImageComponentWithProvider() { }
+        public ImageComponentWithProvider(string link, string providerName)
         {
             this.link = link;
             this.provider = new BaseProvider(providerName);
         }
 
-        public ImageComponent(string link, BaseProvider providerName)
+        public ImageComponentWithProvider(string link, BaseProvider providerName)
         {
             this.link = link;
             this.provider = providerName;
+        }
+    }
+
+    public class ImageComponent : BaseImage
+    {
+        public string link { get; set; }
+        public ImageComponent() { }
+        public ImageComponent(string link)
+        {
+            this.link = link;
         }
     }
 
@@ -33,7 +74,7 @@
 
         public ImageComponentWithId()
         {
-            
+
         }
         public ImageComponentWithId(string id)
         {
---
+++ WhatsAppBussinesApi.Dotnet/Structure/Text/LocationMessage.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WhatsAppBussinesApi.Dotnet.Structure.Text
+{
+    public class LocationMessage : BaseMessage
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TypeMessage type { get; set; } = TypeMessage.location;
+        public LocationComponent location { get; set; }
+
+        public LocationMessage()
+        {
+            
+        }
+        public LocationMessage(string to,LocationComponent location)
+        {
+            this.to = to;
+            this.location = location;
+        }
+
+        public LocationMessage(string to,decimal longitude, decimal latitude, string name, string address)
+        {
+            this.to = to;
+            this.location = new LocationComponent()
+            {
+                address = address,
+                longitude = longitude,
+                latitude = latitude,
+                name = name,
+            };
+        }
+    }
+
+    public class LocationComponent
+    {
+        public decimal longitude { get; set; }
+        public decimal latitude { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}
