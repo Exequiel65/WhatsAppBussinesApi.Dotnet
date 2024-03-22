a5d2a6fbb14b4ee5f8f37b27c2066f1b66348ab7
Exequiel65 <marcos97britos@gmail.com>
2024-03-22 19:11:39 -0300

    change configure initial

---
+++ WhatsAppBussinesApi.Dotnet/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WhatsAppBussinesApi.Dotnet.Client;
+
+namespace WhatsAppBussinesApi.Dotnet
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddWhatsAppBussinesApi(this IServiceCollection services)
+        {
+            services.AddScoped<IWhatsAppBusinessClient,  WhatsAppBusinessClient>();
+
+            return services;
+        }
+    }
+}
